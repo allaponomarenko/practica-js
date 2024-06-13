9da3c54ff1eeb6c49d35a835041ab9e7fe776f4f
allaponomarenko <alla.deyneka03@gmail.com>
2024-06-13 14:56:42 +0200

    estreyas

--- practica-modul-1.js
+++ practica-modul-1.js
@@ -92,3 +92,22 @@ function calcBMI(weight, height) {
     return result.toFixed(1); // методом toFixed() зменшуємо кількість чисел після коми до 1
 }
 */
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const decorBg = document.querySelector('.decor-bg');
+    
+    for (let i = 0; i < 50; i++) {
+      const star = document.createElement('div');
+      star.className = 'star';
+      star.style.top = `${Math.random() * 100}%`;
+      star.style.left = `${Math.random() * 100}%`;
+      star.style.width = `${Math.random() * 3}px`;
+      star.style.height = star.style.width;
+      decorBg.appendChild(star);
+    }
+  });
+  
